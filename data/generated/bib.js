63f9acb3e7791b813b5043b70de2338e49896c1d
bonymjose <49683890+bonymjose@users.noreply.github.com>
2023-05-09 17:24:40 +0100

    Survis commit
    Survis commit

---
+++ data/generated/bib.js
@@ -0,0 +1,95 @@
+﻿const generatedBibEntries = {
+    "Cai_Ge_Liu_Cai_Cham_Yuan_Thalmann_2019, ": {
+        "author": "Cai, Yujun and Ge, Liuhao and Liu, Jun and Cai, Jianfei and Cham, Tat-Jen and Yuan, Junsong and Thalmann, Nadia Magnenat",
+        "doi": "10.1109/iccv.2019.00236",
+        "journal": "2019 IEEE/CVF International Conference on Computer Vision (ICCV)",
+        "keywords": "type:Monocular",
+        "title": "Exploiting spatial-temporal relationships for 3D pose estimation via graph Convolutional Networks",
+        "type": "article",
+        "year": "2019"
+    },
+    "Chen_Ramanan_2017": {
+        "author": "Chen, Ching-Hang and Ramanan, Deva",
+        "doi": "10.1109/cvpr.2017.610",
+        "journal": "2017 IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "keywords": "type:Monocular",
+        "title": "3D human pose estimation",
+        "type": "article",
+        "year": "2017"
+    },
+    "He_Yan_Fragkiadaki_Yu_2020, ": {
+        "author": "He, Yihui and Yan, Rui and Fragkiadaki, Katerina and Yu, Shoou-I",
+        "doi": "10.1109/cvpr42600.2020.00780",
+        "journal": "2020 IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "keywords": "type:Multiview",
+        "title": "Epipolar Transformers",
+        "type": "article",
+        "year": "2020"
+    },
+    "Hu_Zhang_Zhan_Zhang_Wong_2021, ": {
+        "author": "Hu, Wenbo and Zhang, Changgong and Zhan, Fangneng and Zhang, Lei and Wong, Tien-Tsin",
+        "doi": "10.1145/3474085.3475219",
+        "journal": "Proceedings of the 29th ACM International Conference on Multimedia",
+        "keywords": "type:Monocular",
+        "title": "Conditional directed graph convolution for 3D human pose estimation",
+        "type": "article",
+        "year": "2021"
+    },
+    "Iskakov_Burkov_Lempitsky_Malkov_2019, ": {
+        "author": "Iskakov, Karim and Burkov, Egor and Lempitsky, Victor and Malkov, Yury",
+        "doi": "10.1109/iccv.2019.00781",
+        "journal": "2019 IEEE/CVF International Conference on Computer Vision (ICCV)",
+        "keywords": "type:Multiview",
+        "title": "Learnable triangulation of human pose",
+        "type": "article",
+        "year": "2019"
+    },
+    "Martinez_Hossain_Romero_Little_2017, ": {
+        "author": "Martinez, Julieta and Hossain, Rayat and Romero, Javier and Little, James J.",
+        "doi": "10.1109/iccv.2017.288",
+        "journal": "2017 IEEE International Conference on Computer Vision (ICCV)",
+        "keywords": "type:Monocular",
+        "title": "A simple yet effective baseline for 3D human pose estimation",
+        "type": "article",
+        "year": "2017"
+    },
+    "Qiu_Wang_Wang_Wang_Zeng_2019, ": {
+        "author": "Qiu, Haibo and Wang, Chunyu and Wang, Jingdong and Wang, Naiyan and Zeng, Wenjun",
+        "doi": "10.1109/iccv.2019.00444",
+        "journal": "2019 IEEE/CVF International Conference on Computer Vision (ICCV)",
+        "keywords": "type:Multiview",
+        "title": "Cross View Fusion for 3D human pose estimation",
+        "type": "article",
+        "year": "2019"
+    },
+    "Wu_Jin_Liu_Bai_Qian_Liu_Ouyang_2021, ": {
+        "author": "Wu, Size and Jin, Sheng and Liu, Wentao and Bai, Lei and Qian, Chen and Liu, Dong and Ouyang, Wanli",
+        "doi": "10.1109/iccv48922.2021.01096",
+        "journal": "2021 IEEE/CVF International Conference on Computer Vision (ICCV)",
+        "keywords": "type:Multiview",
+        "title": "Graph-based 3D multi-person pose estimation using Multi-View Images",
+        "type": "article",
+        "year": "2021"
+    },
+    "Zhang_Wang_Qiu_Qin_Zeng_2020, ": {
+        "author": "Zhang, Zhe and Wang, Chunyu and Qiu, Weichao and Qin, Wenhu and Zeng, Wenjun",
+        "doi": "10.1007/s11263-020-01398-9",
+        "journal": "International Journal of Computer Vision",
+        "keywords": "type:Multiview",
+        "number": "3",
+        "pages": "703\u2013718",
+        "title": "AdaFuse: Adaptive Multiview Fusion for accurate human pose estimation in the wild",
+        "type": "article",
+        "volume": "129",
+        "year": "2020"
+    },
+    "Zhao_Peng_Tian_Kapadia_Metaxas_2019, ": {
+        "author": "Zhao, Long and Peng, Xi and Tian, Yu and Kapadia, Mubbasir and Metaxas, Dimitris N.",
+        "doi": "10.1109/cvpr.2019.00354",
+        "journal": "2019 IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "keywords": "type:Monocular",
+        "title": "Semantic graph convolutional networks for 3D human pose regression",
+        "type": "article",
+        "year": "2019"
+    }
+};
